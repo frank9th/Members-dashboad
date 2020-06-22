16ce47d271f2eae437a74bad90975462491b0315
MarlineKhavele <marlinekhavele6@gmail.com>
2020-06-22 19:52:43 +0300

    Added urls WIP models

--- leadboard/views.py
+++ leadboard/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {"something":Leadership.objects.all()}
+    return render(request, "home.html", context)
+
+def about(request):
+    context = {"something": Leadership.objects.all()}
+    return render(request, "about.html", context)
