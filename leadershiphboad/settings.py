cce7f2be99b9b83e278459e59cee8db7b3e974c5
frank9th <ap.teamservices@gmail.com>
2020-06-23 03:37:30 +0100

    updated the app

--- leadershiphboad/settings.py
+++ leadershiphboad/settings.py
@@ -120,4 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+
+]
+
 REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'}
